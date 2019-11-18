7afb241ca5af10164a034c9af0729f083d9a3958
Jeffrey Brown <brownjs03@gmail.com>
2019-11-18 04:49:09 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -18,3 +18,17 @@ class Appointment
 
 end 
 =end 
+
+class Appointment
+  attr_accessor :date, :doctor, :patient
+  @@all = []
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
